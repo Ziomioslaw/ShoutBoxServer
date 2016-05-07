87837c1627e0d1ec7ef1e58af5c9d1a98add5bbd
Ziomioslaw <ziomioslaw@wp.pl>
2016-05-07 09:52:55 +0200

    Implement base for POST /shout

--- api.php
+++ api.php
@@ -46,6 +46,32 @@ $application->route('GET /shout', function() use($application, $shoutBoxTableNam
     return $application->_json($sth->fetchAll(PDO::FETCH_ASSOC));
 });
 
+$application->route('POST /shout', function() use($application, $shoutBoxTableName) {
+    $request = $application->request();
+    $memberID = $request->data->memberID;
+    $memberName = $request->data->memberName;
+    $message = $request->data->message;
+
+    $sth = $this->pdo->prepare("INSERT INTO {$shoutBoxTableName} (
+            `ID_MEMBER`,
+            `displayname`,
+            `message`,
+            `time`
+        ) VALUES (
+            :memberID,
+            :memberName,
+            :message,
+            UNIX_TIMESTAMP()
+        )");
+
+    $sth->bindParam(':memberID', $memberID, PDO::PARAM_INT);
+    $sth->bindParam(':memberName', $memberName, PDO::PARAM_STR);
+    $sth->bindParam(':message', $message, PDO::PARAM_STR);
+    $sth->execute();
+
+    return $this->pdo->lastInsertId();
+});
+
 $application->map('error', function(Exception $ex) use($application) {
     $application->_error($ex);
 });
