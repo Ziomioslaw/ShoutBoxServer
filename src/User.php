31cc845743bce2e226e14ba16200663edff4dba9
Ziomioslaw <ziomioslaw@wp.pl>
2016-05-14 08:46:14 +0200

    Extend User class with additional data

--- src/User.php
+++ src/User.php
@@ -9,4 +9,12 @@ class User {
     public function isAdmin() {
         return $this->data['is_admin'];
     }
+
+    public function getID() {
+        return $this->data['id'];
+    }
+
+    public function getName() {
+        return $this->data['name']; // it can be also 'username'
+    }
 }
